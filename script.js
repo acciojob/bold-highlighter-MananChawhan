d23d56686bdee8eb7b86eba328a5c146a5fc8290
MananChawhan <121217947+MananChawhan@users.noreply.github.com>
2025-06-09 11:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ function highlight() {
 
 
 function return_normal() {
-    //Write your code here
+    function highlight() {
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(el => {
+        el.style.color = 'rgb(0, 128, 0)'; // green
+    });
+}
+
+function return_normal() {
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(el => {
+        el.style.color = 'rgb(0, 0, 0)'; // black
+    });
+}
+
 
     
 }
